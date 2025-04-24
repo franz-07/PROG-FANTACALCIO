5f9eec8b4ebaed115126dc7e30724333babfcdb1
AlmaBilli <billialma110@gmail.com>
2025-04-24 09:41:06 +0200

    modifica (ancora) leghe

--- schermata_leghe.py
+++ schermata_leghe.py
@@ -10,30 +10,31 @@ class SchermataLeghe(ctk.CTkFrame):
         self.master = master
         self.username = username
 
-        # Display username and logout button
         ctk.CTkLabel(self, text=f"Loggato come: {username}!", font=("Poppins", 12)).pack(anchor="ne", padx=5, pady=5)
         ctk.CTkButton(self, text="Esci", command=self.logout).pack(anchor="ne", padx=5, pady=5)
 
-        # Title label
-        ctk.CTkLabel(self, text="Scegli la tua lega", font=("Poppins", 18)).pack(pady=20)
-
-        # Display images
+        ctk.CTkLabel(self, text="Scegli la tua lega", font=("Poppins", 24, "bold")).pack(pady=20)
+        
         self.mostra_immagini()
 
     def mostra_immagini(self):
         try:
-    
+        # Creazione delle immagini
             img1 = ctk.CTkImage(Image.open("immagini/immagine1_lega.png"), size=(200, 200))
             img2 = ctk.CTkImage(Image.open("immagini/immagine2_lega.png"), size=(200, 200))
+            img3 = ctk.CTkImage(Image.open("immagini/immagine3_lega.png"), size=(200, 200))
 
-    
-            ctk.CTkButton(self, text="", image=img1, command=self.lega1).pack(side="left", padx=10, pady=10)
-            ctk.CTkButton(self, text="", image=img2, command=self.lega2).pack(side="right", padx=10, pady=10)
+        # Disposizione orizzontale delle immagini/bottoni
+            button_frame = ctk.CTkFrame(self)  # Frame principale per i bottoni
+            button_frame.pack(pady=20)
+
+            ctk.CTkButton(button_frame, text="", image=img1, command=self.lega1).pack(side="left", padx=10)
+            ctk.CTkButton(button_frame, text="", image=img2, command=self.lega2).pack(side="left", padx=10)
+            ctk.CTkButton(button_frame, text="", image=img3, command=self.lega3).pack(side="left", padx=10)
 
             ctk.CTkButton(self, text="creazione lega", command=self.crea_lega).pack(anchor="ne", padx=10, pady=10)
         except Exception as e:
             ctk.CTkLabel(self, text=f"Errore nel caricamento delle immagini: {e}").pack()
-
     def logout(self):
         self.master.mostra_schermata_login()
 
@@ -43,5 +44,10 @@ class SchermataLeghe(ctk.CTkFrame):
     def lega2(self):
         self.master.mostra_schermata_home(self.username, lega="lega2")
 
+<<<<<<< Updated upstream
     def crea_lega(self):
         self.master.mostra_schermata_crea_lega(self.username)
+=======
+    def lega3(self):
+        self.master.mostra_schermata_home(self.username, lega="crea la tua lega")
+>>>>>>> Stashed changes
